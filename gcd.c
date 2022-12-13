05d87dd7e2944f40ab491722079728e99bae4dbc
alexnda <97135325+kenlies@users.noreply.github.com>
2022-12-13 08:25:45 +0200

    Add files via upload

---
+++ gcd.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+// using iterative substraction
+int	gcd(int a, int b)
+{
+	if (!a || !b)
+		return 0;
+
+	while (b)
+	{
+		if (a > b)
+			a = a - b;
+		else
+			b = b - a;
+	}
+	return a;
+}
+
+// using recursive substraction
+int	gcd(int a, int b)
+{
+	if (b == 0)
+		return a;
+	else if (a > b)
+		return gcd(a - b, b);
+	else
+		return gcd(a, b - a);
+}
+/*
+int main ()
+{
+	int a;
+	int b;
+
+	printf("Please provide number 1 : ");
+	scanf("%d", &a);
+	printf("Please provide number 2 : ");
+	scanf("%d", &b);
+	
+	printf("Highest common divisor = %d", gcd(a, b));
+
+	return 0;	
+}
+*/
